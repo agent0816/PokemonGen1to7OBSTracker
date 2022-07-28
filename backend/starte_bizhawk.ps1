e33ad1308bb8d7849bc8cad3428747e3bbdf8531
agent0816 <plakitst-dd@web.de>
2022-07-28 16:26:41 +0200

    Fahrplan erstellt

---
+++ backend/starte_bizhawk.ps1
@@ -0,0 +1,3 @@
+Set-Location D:\Emulatoren\Bizhawk
+$lua = $env:APPDATA + "\TouchPortal\plugins\Bildquellenwechsel\obsautomation.lua"
+.\EmuHawk.exe --socket_ip=127.45.45.45 --socket_port=43885 --lua=$lua

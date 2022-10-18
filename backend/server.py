726646a1d27f77a9bd42b3239aae9678e5460bd4
IntegerOverFlori <20774724+IntegerOverFlori@users.noreply.github.com>
2022-10-18 13:58:50 +0200

    enabled animated sprites for emerald and gen 4

--- backend/server.py
+++ backend/server.py
@@ -84,7 +84,7 @@ def get_sprite(pokemon, anim, edition):
         female = 'female/' 
     else:
         female = ''
-    if anim and edition in (51, 52, 53, 54):
+    if anim and edition in (33, 41, 42, 43, 44, 45, 51, 52, 53, 54):
         filetype = '.gif'
         animated = 'animated/'
     else:

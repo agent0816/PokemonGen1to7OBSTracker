59313d3210e731c072e856024da22be473ff29a2
agent0816 <plakitst-dd@web.de>
2022-10-20 22:28:27 +0200

    LUTs aus dem Script in eine Datei verfrachtet

--- backend/server.py
+++ backend/server.py
@@ -55,28 +55,34 @@ async def changeSource(player, slots, team, edition):
         for ws in websockets:
             await ws.call_batch(batch)
 
-edition_lut = {
-    11: 'red',
-    12: 'red',
-    13: 'yellow',
-    21: 'gold',
-    22: 'silver',
-    23: 'crystal',
-    31: 'ruby',
-    32: 'ruby',
-    33: 'emerald',
-    34: 'firered',
-    35: 'firered',
-    41: 'diamond',
-    42: 'diamond',
-    43: 'platinum',
-    44: 'heartgold',
-    45: 'heartgold',
-    51: 'black',
-    52: 'black',
-    53: 'black',
-    54: 'black'}
-female_lut = {3,12,19,20,25,26,32,29,41,42,44,45,64,65,84,85,97,111,112,118,119,123,129,130,133,154,165,166,178,185,186,190,194,195,198,202,203,207,208,212,214,215,215,217,221,224,229,232,255,256,257,267,269,272,274,275,307,308,315,316,317,322,323,332,350,369,396,397,398,399,400,401,402,403,404,405,407,415,417,418,419,424,443,444,445,449,450,453,454,456,457,459,460,461,464,465,473,521,592,593,668,678,876,902}
+luts = yaml.safe_load(open('backend/data/luts.yml'))
+
+edition_lut = luts['edition_lut']
+
+female_lut = luts['female_lut']
+
+# edition_lut = {
+#     11: 'red',
+#     12: 'red',
+#     13: 'yellow',
+#     21: 'gold',
+#     22: 'silver',
+#     23: 'crystal',
+#     31: 'ruby',
+#     32: 'ruby',
+#     33: 'emerald',
+#     34: 'firered',
+#     35: 'firered',
+#     41: 'diamond',
+#     42: 'diamond',
+#     43: 'platinum',
+#     44: 'heartgold',
+#     45: 'heartgold',
+#     51: 'black',
+#     52: 'black',
+#     53: 'black',
+#     54: 'black'}
+# female_lut = {3,12,19,20,25,26,32,29,41,42,44,45,64,65,84,85,97,111,112,118,119,123,129,130,133,154,165,166,178,185,186,190,194,195,198,202,203,207,208,212,214,215,215,217,221,224,229,232,255,256,257,267,269,272,274,275,307,308,315,316,317,322,323,332,350,369,396,397,398,399,400,401,402,403,404,405,407,415,417,418,419,424,443,444,445,449,450,453,454,456,457,459,460,461,464,465,473,521,592,593,668,678,876,902}
 def get_sprite(pokemon, anim, edition):
     
     shiny = 'shiny/' if pokemon.shiny else ''
@@ -141,18 +147,18 @@ async def handle_client(reader, writer):
             msg = await reader.read(1320)
             team = pokedecoder.team(msg, 5)
 
-        logging.debug(f'{team=}')
+        logging.debug(f'{team=}') # type: ignore
         if teams[player] == []:
-            teams[player] = team
-            await changeSource(player, range(6), team, edition)
+            teams[player] = team # type: ignore
+            await changeSource(player, range(6), team, edition) # type: ignore
 
         else:
             diff =[]
             for i in range(6):
-                if teams[player][i] != team[i]:
+                if teams[player][i] != team[i]: # type: ignore
                     diff.append(i)
-            teams[player] = team
-            await changeSource(player, diff, team, edition)
+            teams[player] = team # type: ignore
+            await changeSource(player, diff, team, edition) # type: ignore
     writer.close()
 
 async def start():

58267ec8af9a1d91bcdb4917f2e0a38170428d3f
agent0816 <plakitst-dd@web.de>
2023-02-23 16:56:24 +0100

    Trackerskript fertig gestellt

--- startTracker.ps1
+++ startTracker.ps1
@@ -11,9 +11,14 @@ if($python -ne "Python 3.10.10"){
     Remove-Item -Path ".\tmp" -Recurse
 }
 
+$machinePath = [System.Environment]::GetEnvironmentVariable("Path", "Machine")
+$userPath = [System.Environment]::GetEnvironmentVariable("Path", "User")
+$env:Path = "$machinePath;$userPath"
+
 python -m pip install --upgrade pip
 $yaml = $true
 $kivy = $true
+$obsws = $true
 
 $installed = (python -m pip list).split("`n")
 for($i = 0; $i -lt $installed.Length; $i++){
@@ -24,6 +29,19 @@ for($i = 0; $i -lt $installed.Length; $i++){
     if($kivy -eq $true){
         $kivy = -not ($package -eq "Kivy")
     }
+    if($obsws -eq $true){
+        $obsws = -not ($package -eq "simpleobsws")
+    }
+}
+
+if($yaml){
+    python -m pip install PyYAML
+}
+if($kivy){
+    python -m pip install Kivy
+}
+if($obsws){
+    python -m pip install simpleobsws
 }
 
 python .\main.py

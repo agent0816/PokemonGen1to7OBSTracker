53231971f6974ea8c3062f9d405a1ba0f2ea32d0
agent0816 <plakitst-dd@web.de>
2023-04-12 18:19:35 +0200

    Ordnerinitialosierung komplett verschoben

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 from pathlib import Path
-Path('logs').mkdir(parents=True, exist_ok=True)
 import logging
 logging.basicConfig(level=logging.INFO, filename='logs/server.log',format='[%(asctime)s] %(levelname)s: %(message)s', filemode='w', encoding='utf-8')
 import frontend.app as FEApp
 import asyncio
 import initialize_tree as init
 
-init.init_logging_folder()
-init.init_config_folder()
-
 def main():
     app = FEApp.TrackerApp()
     asyncio.run(app.async_run())
 
 if __name__ == '__main__':
+    init.init_logging_folder()
+    init.init_config_folder()
     main()

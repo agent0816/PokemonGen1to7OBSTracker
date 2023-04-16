897f2d574d6e3ead0a211dbbdd22973e0ad99bea
agent0816 <plakitst-dd@web.de>
2023-04-16 21:21:13 +0200

    main angepasst

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from pathlib import Path
+import initialize_tree as init
+init.init_logging_folder()
 import logging
 logging.basicConfig(level=logging.INFO, filename='logs/server.log',format='[%(asctime)s] %(levelname)s: %(message)s', filemode='w', encoding='utf-8')
 import frontend.app as FEApp
 import asyncio
-import initialize_tree as init
 
 def main():
     app = FEApp.TrackerApp()
     asyncio.run(app.async_run())
 
 if __name__ == '__main__':
-    init.init_logging_folder()
     init.init_config_folder()
     main()

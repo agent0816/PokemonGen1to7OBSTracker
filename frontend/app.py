ce774c5e4cafb020d310b8662b3607d421356f0d
agent0816 <plakitst-dd@web.de>
2023-08-11 14:56:44 +0200

    globale Variablen im Froend entfernt

--- frontend/app.py
+++ frontend/app.py
@@ -13,9 +13,9 @@ import backend.munchlax as client
 import logging
 
 logger = logging.getLogger(__name__)
-logging_formatter = logging.Formatter('[%(asctime)s] %(levelname)s: %(message)s')
+logging_formatter = logging.Formatter("[%(asctime)s] %(levelname)s: %(message)s")
 
-file_handler = logging.FileHandler('logs/frontend.log', 'w')
+file_handler = logging.FileHandler("logs/frontend.log", "w")
 file_handler.setFormatter(logging_formatter)
 logger.addHandler(file_handler)
 
@@ -24,74 +24,121 @@ stream_handler.setFormatter(logging_formatter)
 logger.addHandler(stream_handler)
 
 
-Config.read('gui.ini')
-connector = None
-OBSconnector = None
-clientConnector = None
-bizConnector = None
-connectors = []
+Config.read("gui.ini")
+
 
 class Screens(ScreenManager):
-    def __init__(self, **kwargs):
+    def __init__(
+        self,
+        connector,
+        OBSconnector,
+        clientConnector,
+        bizConnector,
+        connectors,
+        externalIPv4,
+        externalIPv6,
+        configsave,
+        sp,
+        rem,
+        obs,
+        bh,
+        pl,
+        **kwargs,
+    ):
         super().__init__(**kwargs)
         self.transition = FadeTransition()
-        self.add_widget(MainMenu(connector, OBSconnector, clientConnector, bizConnector, connectors, configsave, sp, rem, obs, bh, pl))
-        self.add_widget(SettingsMenu(externalIPv4, externalIPv6, configsave, sp, rem, obs, bh, pl))
+        self.add_widget(
+            MainMenu(
+                connector,
+                OBSconnector,
+                clientConnector,
+                bizConnector,
+                connectors,
+                configsave,
+                sp,
+                rem,
+                obs,
+                bh,
+                pl,
+            )
+        )
+        self.add_widget(
+            SettingsMenu(externalIPv4, externalIPv6, configsave, sp, rem, obs, bh, pl)
+        )
         self.current = "MainMenu"
 
-class TrackerApp(App):  
+
+class TrackerApp(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         Window.bind(on_request_close=self.exit_check)
-        Config.set('graphics', 'resizable', 1)
-        Config.set('graphics', 'width', "600")
-        Config.set('graphics', 'height', "400")
-        Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
+        Config.set("graphics", "resizable", 1)
+        Config.set("graphics", "width", "600")
+        Config.set("graphics", "height", "400")
+        Config.set("input", "mouse", "mouse,multitouch_on_demand")
+        self.connector = None
+        self.OBSconnector = None
+        self.clientConnector = None
+        self.bizConnector = None
+        self.connectors = []
 
     def build(self):
-        global externalIPv4
         try:
-            externalIPv4 = requests.get('https://ifconfig.me/ip', timeout=1).text
+            self.externalIPv4 = requests.get("https://ifconfig.me/ip", timeout=1).text
         except requests.exceptions.Timeout:
-            externalIPv4 = ''
-        global externalIPv6
-        command = "(Get-NetIPAddress -AddressFamily IPv6 | Where-Object -Property PrefixOrigin -eq \'Dhcp\').IPAddress"
-        process = subprocess.Popen(["powershell.exe",command], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            self.externalIPv4 = ""
+        command = "(Get-NetIPAddress -AddressFamily IPv6 | Where-Object -Property PrefixOrigin -eq 'Dhcp').IPAddress"
+        process = subprocess.Popen(
+            ["powershell.exe", command],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=True,
+        )
         stdout, stderr = process.communicate()
-        externalIPv6 = stdout.decode()
-        global configsave
-        configsave = 'backend/config/'
-        global bh
-        bh = {}
-        with open(f"{configsave}bh_config.yml") as file:
-            bh = yaml.safe_load(file)
-        global obs
-        obs = {}
-        with open(f"{configsave}obs_config.yml") as file:
-            obs = yaml.safe_load(file)
-        global sp
-        sp = {}
-        with open(f"{configsave}sprites.yml") as file:
-            sp = yaml.safe_load(file)
-        client.conf = sp
-        global pl
-        pl = {}
-        with open(f"{configsave}player.yml") as file:
-            pl = yaml.safe_load(file)
-        global rem
-        rem = {}
-        with(open(f"{configsave}remote.yml")) as file:
-            rem = yaml.safe_load(file)
+        self.externalIPv6 = stdout.decode()
+        self.configsave = "backend/config/"
+        self.bh = {}
+        with open(f"{self.configsave}bh_config.yml") as file:
+            self.bh = yaml.safe_load(file)
+        self.obs = {}
+        with open(f"{self.configsave}obs_config.yml") as file:
+            self.obs = yaml.safe_load(file)
+        self.sp = {}
+        with open(f"{self.configsave}sprites.yml") as file:
+            self.sp = yaml.safe_load(file)
+        client.conf = self.sp
+        self.pl = {}
+        with open(f"{self.configsave}player.yml") as file:
+            self.pl = yaml.safe_load(file)
+        self.rem = {}
+        with open(f"{self.configsave}remote.yml") as file:
+            self.rem = yaml.safe_load(file)
 
-        return Screens()
+        arguments = [
+            self.connector,
+            self.OBSconnector,
+            self.clientConnector,
+            self.bizConnector,
+            self.connectors,
+            self.externalIPv4,
+            self.externalIPv6,
+            self.configsave,
+            self.sp,
+            self.rem,
+            self.obs,
+            self.bh,
+            self.pl,
+        ]
+
+        return Screens(*arguments)
 
     def exit_check(self, *args, **kwargs):
-        self.save_config(f"{configsave}bh_config.yml", bh)
-        self.save_config(f"{configsave}obs_config.yml", obs)
-        self.save_config(f"{configsave}sprites.yml", sp)
-        self.save_config(f"{configsave}player.yml", pl)
-        self.save_config(f"{configsave}remote.yml", rem)
+        self.save_config(f"{self.configsave}bh_config.yml", self.bh)
+        self.save_config(f"{self.configsave}obs_config.yml", self.obs)
+        self.save_config(f"{self.configsave}sprites.yml", self.sp)
+        self.save_config(f"{self.configsave}player.yml", self.pl)
+        self.save_config(f"{self.configsave}remote.yml", self.rem)
 
     def save_config(self, path, setting):
-        with open(path, 'w') as file:
+        with open(path, "w") as file:
             yaml.dump(setting, file)

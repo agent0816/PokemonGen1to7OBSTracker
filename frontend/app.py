79fa8aeedf9a9857c0680dc0fb8b58a84f32bbfb
agent0816 <plakitst-dd@web.de>
2023-09-01 17:17:45 +0200

    ich habs bald

--- frontend/app.py
+++ frontend/app.py
@@ -3,6 +3,7 @@ import sys
 import subprocess
 import yaml
 import requests
+import logging
 from frontend.widgets.mainmenu import MainMenu
 from frontend.widgets.settingsmenu import SettingsMenu
 from kivy.app import App
@@ -12,13 +13,15 @@ from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
+from kivy.uix.progressbar import ProgressBar
 from kivy.uix.screenmanager import FadeTransition
 from kivy.uix.screenmanager import ScreenManager
+from pyupdater.client import Client
+from client_config import ClientConfig
 from backend.classes.arceus import Arceus
 from backend.classes.bizhawk import Bizhawk
 from backend.classes.munchlax import Munchlax
 from backend.classes.obs import OBS
-import logging
 
 logger = logging.getLogger(__name__)
 logging_formatter = logging.Formatter("[%(asctime)s] %(levelname)s: %(message)s")
@@ -33,6 +36,9 @@ logger.addHandler(stream_handler)
 
 
 Config.read("gui.ini")
+
+APP_NAME = 'PokemonOBSTracker'
+APP_VERSION = '0.1'
 
 class Screens(ScreenManager):
     def __init__(
@@ -96,6 +102,8 @@ class TrackerApp(App):
         Config.set("input", "mouse", "mouse,multitouch_on_demand")
 
     def build(self):
+        self.check_for_update()
+        
         try:
             self.externalIPv4 = requests.get("https://ipinfo.io/ip", timeout=1).text
         except requests.exceptions.Timeout:
@@ -160,6 +168,53 @@ class TrackerApp(App):
         ]
 
         return Screens(*arguments)
+
+    def check_for_update(self):
+        client = Client(ClientConfig(), refresh=True)
+        logger.info(client)
+        client.add_progress_hook(self.print_status_info)
+        app_update = client.update_check(APP_NAME, APP_VERSION)
+        logger.info(app_update)
+        if app_update:
+            self.show_update_popup(app_update)
+
+    def show_update_popup(self, app_update):
+        box = BoxLayout(orientation='vertical')
+        box.add_widget(Label(text='Update verfügbar!'))
+
+        self.progress_bar = ProgressBar(max=100)
+        box.add_widget(self.progress_bar)
+
+        btn_layout = BoxLayout()
+        download_btn = Button(text='Download')
+        download_btn.bind(on_press=lambda x: self.download_update(app_update)) # type: ignore
+        btn_layout.add_widget(download_btn)
+
+        cancel_btn = Button(text='Abbrechen')
+        cancel_btn.bind(on_press=lambda x: x.parent.parent.dismiss()) # type: ignore
+        btn_layout.add_widget(cancel_btn)
+
+        box.add_widget(btn_layout)
+
+        self.popup = Popup(title='Update verfügbar!', content=box,
+                           size_hint=(None, None), size=(400, 300))
+        self.popup.open()
+
+    def download_update(self, app_update):
+        app_update.download()
+        if app_update.is_downloaded():
+            app_update.extract_restart()
+
+    def print_status_info(self, info):
+        total = info.get(u'total')
+        downloaded = info.get(u'downloaded')
+        status = info.get(u'status')
+        print(downloaded, total, status)
+        
+        # Aktualisieren des ProgressBar-Werts
+        if total:
+            percentage = (downloaded / total) * 100
+            self.progress_bar.value = percentage
 
     def exit_check(self, *args, **kwargs):
         self.save_config(f"{self.configsave}bh_config.yml", self.bh)

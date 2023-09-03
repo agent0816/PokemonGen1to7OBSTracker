3fee76e0c9136167f763f81c9fad6ca6b602e384
agent0816 <plakitst-dd@web.de>
2023-09-03 21:07:10 +0200

    app-version 0.2

--- frontend/app.py
+++ frontend/app.py
@@ -32,7 +32,7 @@ logger.addHandler(stream_handler)
 Config.read("gui.ini")
 
 APP_NAME = 'PokemonOBSTracker'
-APP_VERSION = '0.1'
+APP_VERSION = '0.2'
 
 class Screens(ScreenManager):
     def __init__(

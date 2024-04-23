6fbf9f1feca3a0071b3ed498eda84afe134c9a11
agent0816 <plakitst-dd@web.de>
2024-04-23 09:29:06 +0200

    Revert "ver 0.6.2"
    This reverts commit a2ca5eedb277924403528274ee3505a8f1c3f5e8.

--- frontend/app.py
+++ frontend/app.py
@@ -35,7 +35,7 @@ stream_handler.setFormatter(logging_formatter)
 logger.addHandler(stream_handler)
 
 APP_NAME = "PokemonOBSTracker"
-APP_VERSION = "0.6.2"
+APP_VERSION = "0.6.1"
 
 
 class Screens(ScreenManager):

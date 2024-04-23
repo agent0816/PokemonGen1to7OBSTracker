d063e5df4229f305a3c9eb2f9a925a8b1966a338
agent0816 <plakitst-dd@web.de>
2024-04-23 09:31:18 +0200

    Revert "- citrahandler erstellt"
    This reverts commit 593392949281a8ef6019798a724a578de451c4d2.

--- backend/classes/arceus.py
+++ backend/classes/arceus.py
@@ -104,49 +104,17 @@ class Arceus:
                 del self.munchlax_heartbeats[client_id]
                 del self.heartbeat_counts[client_id]
 
-    # async def send_message(self, writer, message):
-    #     serialized_message = pickle.dumps(message)
-    #     length = len(serialized_message).to_bytes(4, 'big')
-    #     writer.write(length)
-    #     await writer.drain()
-    #     writer.write(serialized_message)
-    #     await writer.drain()
-    
     async def send_message(self, writer, message):
         serialized_message = pickle.dumps(message)
-        CHUNK_SIZE = 500  # Die Größe jedes Chunks in Bytes
-
-        # Gesamtlänge der Nachricht senden
         length = len(serialized_message).to_bytes(4, 'big')
         writer.write(length)
         await writer.drain()
-
-        # Nachricht in Chunks senden
-        for i in range(0, len(serialized_message), CHUNK_SIZE):
-            chunk = serialized_message[i:i+CHUNK_SIZE]
-            # Größe des aktuellen Chunks senden
-            chunk_length = len(chunk).to_bytes(4, 'big')
-            writer.write(chunk_length)
-            await writer.drain()
-            # Chunk senden
-            writer.write(chunk)
-            await writer.drain()
-
-
-    # async def receive_message(self, reader):
-    #     message_length = int.from_bytes(await reader.read(4), 'big')
-    #     message = await reader.read(message_length)
-
-    #     return pickle.loads(message)
-
+        writer.write(serialized_message)
+        await writer.drain()
+    
     async def receive_message(self, reader):
-        total_length = int.from_bytes(await reader.read(4), 'big')
-        message = b''
-
-        while len(message) < total_length:
-            chunk_length = int.from_bytes(await reader.read(4), 'big')
-            chunk = await reader.read(chunk_length)
-            message += chunk
+        message_length = int.from_bytes(await reader.read(4), 'big')
+        message = await reader.read(message_length)
 
         return pickle.loads(message)
     

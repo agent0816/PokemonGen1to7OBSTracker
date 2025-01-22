a580a6575cc92d37b9cfdd45d72cac5dee87df37
agent0816 <plakitst-dd@web.de>
2025-01-22 17:04:03 +0100

    version 0.7.4

--- frontend/app.py
+++ frontend/app.py
@@ -36,7 +36,7 @@ stream_handler.setFormatter(logging_formatter)
 logger.addHandler(stream_handler)
 
 APP_NAME = "PokemonOBSTracker"
-APP_VERSION = "0.7.3"
+APP_VERSION = "0.7.4"
 
 
 class Screens(ScreenManager):
